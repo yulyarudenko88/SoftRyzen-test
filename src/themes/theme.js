63b19195fe80182d4c87bb4c71fd86ddbccba308
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-11-29 15:14:52 +0200

    add: theme

---
+++ src/themes/theme.js
@@ -0,0 +1,49 @@
+export const theme = {
+  colors: {
+    primaryColor: '#173D33',
+    accentColor: '#97D28B',
+    whiteColor: '#fff',
+    backgroundColor: '#F3F5FA',
+    secondaryBGColor: '#EAEDF1',
+    btnBGColor: '#DCEFD8',
+    mobileMenuBGColor: 'rgba(23, 61, 51, 0.75)',
+    headerBGColor: '#fff',
+    backdropColor: 'rgba(23, 61, 51, 0.25)',
+    primaryTextColor: '#173D33',
+    secondaryTextColor: 'rgba(255, 255, 255, 0.25)',
+  },
+
+  transforms: {
+    transitionDuration: '250ms',
+    timingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+
+  // fontSizes: {
+  //   8: 8,
+  //   10: 10,
+  //   12: 12,
+  //   14: 14,
+  //   16: 16,
+  //   18: 18,
+  //   24: 24,
+  //   28: 28,
+  //   32: 32,
+  //   44: 44,
+  //   60: 60,
+  //   72: 72,
+  //   100: 100,
+  // },
+
+  borders: {
+    line: '1px solid #97D28B',
+    primaryBtn: '1px solid #173D33',
+    secondaryBtn: '1px solid #97D28B',
+  },
+};
