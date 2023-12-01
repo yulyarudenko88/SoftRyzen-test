d6d726cc89cccc856aeefbcb82df0373dd909597
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-12-01 19:52:12 +0200

    add: electricity section

--- src/components/ElectricitySection/ElectricitySection.jsx
+++ src/components/ElectricitySection/ElectricitySection.jsx
@@ -1,46 +1,38 @@
+import { useState, useEffect } from "react";
 import Container from "../Container/Container.jsx";
 import SectionTitle from "../SectionTitle/SectionTitle.jsx";
-// import ValuesGallery from "../ValuesGallery/ValuesGallery.jsx";
-// import MainBtn from "../MainBtn/MainBtn.jsx";
-import { Section } from "./ElectricitySection.styled.js";
+import { Section, Counter } from "./ElectricitySection.styled.js";
 
-// import MobileWindTurbine from "../../assets/images/AboutSection/mobile_wind-turbine.jpg";
-// import MobileWindTurbineRetina from "../../assets/images/AboutSection/mobile_wind-turbine@2x.jpg";
-// import TabletWindTurbine from "../../assets/images/AboutSection/tablet_wind-turbine.jpg";
-// import TabletWindTurbineRetina from "../../assets/images/AboutSection/tablet_wind-turbine@2x.jpg";
-// import DesktopWindTurbine from "../../assets/images/AboutSection/desktop_wind-turbine.jpg";
-// import DesktopWindTurbineRetina from "../../assets/images/AboutSection/desktop_wind-turbine@2x.jpg";
+const storedCount = localStorage.getItem("electricityCount");
+const initialCount = storedCount ? parseInt(storedCount, 10) : 1134147814;
 
 const ElectricitySection = () => {
+  const [counter, setCounter] = useState(initialCount);
+
+  useEffect(() => {
+    // increasing counter every second and writing counter value to local storage
+    const intervalId = setInterval(() => {
+      setCounter((prevCount) => {
+        const newCount = prevCount + 1;
+        localStorage.setItem("electricityCount", newCount.toString());
+        return newCount;
+      });
+    }, 1000);
+
+    //clearing interval when component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <Section>
       <Container>
-        <SectionTitle textAlign = 'center'>Electricity we produced
-for all time</SectionTitle>
-        {/* <Picture>
-          <source
-            media="(min-width: 1280px)"
-            srcSet={`${DesktopWindTurbine}, ${DesktopWindTurbineRetina} 2x`}
-          />
-          <source
-            media="(min-width: 768px)"
-            srcSet={`${TabletWindTurbine}, ${TabletWindTurbineRetina} 2x`}
-          />
-          <img
-            src={MobileWindTurbine}
-            srcSet={`${MobileWindTurbine}, ${MobileWindTurbineRetina} 2x`}
-            alt="Wind turbine - clean energy"
-          />
-        </Picture>
-        <SectionTitle>Main values of our company</SectionTitle>
-        <Desc>
-          EcoSolution envisions a world where sustainable energy solutions power
-          a brighter and cleaner future for all. We aspire to be at the
-          forefront of the global shift towards renewable energy, leading the
-          way in innovative technologies that harness the power of nature to
-          meet the world's energy needs.
-        </Desc>
-        <ValuesGallery /> */}
+        <SectionTitle textAlign="center">
+          Electricity we produced for all time
+        </SectionTitle>
+        <Counter>
+          {counter.toLocaleString()}
+          <span>kWh</span>
+        </Counter>
       </Container>
     </Section>
   );
--- src/components/ElectricitySection/ElectricitySection.styled.js
+++ src/components/ElectricitySection/ElectricitySection.styled.js
@@ -6,5 +6,39 @@ export const Section = styled.section`
 `;
 
 export const Title = styled(SectionTitle)`
-text-align: center;
+  text-align: center;
+`;
+
+export const Counter = styled.p`
+  font-family: ${({ theme }) => theme.fontFamilies.headersFontFamily};
+  font-size: 48px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  color: ${({ theme }) => theme.colors.accentColor};
+  line-height: 1;
+  text-transform: uppercase;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  padding-top: 96px;
+  position: relative;
+
+  span {
+    color: ${({ theme }) => theme.colors.primaryTextColor};
+    font-size: 24px;
+    font-weight: ${({ theme }) => theme.fontWeights.regular};
+    line-height: 1;
+    text-transform: capitalize;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 24px;
+    left: 50%;
+    width: 1px;
+    height: 48px;
+    background-color: ${({ theme }) => theme.colors.accentColor};
+  }
 `;
