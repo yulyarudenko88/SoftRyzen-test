f07b1b341e8ff86d3c2612b08434204b82c90ea2
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-12-01 22:31:39 +0200

    add: markup for slider's gallery

--- src/components/SliderGallery/SliderGallery.jsx
+++ src/components/SliderGallery/SliderGallery.jsx
@@ -1,8 +1,21 @@
+import { slides } from "../../assets/data/sliders.js";
+import SliderCard from "../SliderCard/SliderCard.jsx";
 
-const SliderCard = () => {
-  return <ul>
-
-  </ul>;
+const SliderGallery = () => {
+  return (
+    <ul>
+      {slides.map(({ src, srcRetina, title, shortDesc, time }) => (
+        <SliderCard
+          key={title}
+          src={src}
+          srcRetina={srcRetina}
+          title={title}
+          shortDesc={shortDesc}
+          time={time}
+        />
+      ))}
+    </ul>
+  );
 };
 
-export default SliderCard;
+export default SliderGallery;
